0b8b0b96cf623cb1a48371228406d2bc630bffaa
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-08 17:12:59 +0200

    [Editor] Add basic UI

---
+++ sources/xplat-editor/Stride.Avalonia.GameStudio/Views/AboutWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class AboutWindow : Window
+{
+    public AboutWindow()
+    {
+        InitializeComponent();
+    }
+}
