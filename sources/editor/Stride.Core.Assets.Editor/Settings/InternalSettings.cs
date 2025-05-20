8ecf9259576052445677ffd226c7529ee438bd7a
Nicolas Musset <nicolas@color-rise.xyz>
2025-05-20 17:24:36 +0200

    [Editor] Define MRU settings key

--- sources/editor/Stride.Core.Assets.Editor/Settings/InternalSettings.cs
+++ sources/editor/Stride.Core.Assets.Editor/Settings/InternalSettings.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Stride.Core.MostRecentlyUsedFiles;
 using Stride.Core.Settings;
 
 namespace Stride.Core.Assets.Editor.Settings;
@@ -9,18 +10,27 @@ public static class InternalSettings
 {
     private static readonly SettingsProfile profile;
 
+    public static SettingsContainer SettingsContainer { get; } = new();
+
+    public static readonly SettingsKey<MRUDictionary> MostRecentlyUsedSessions = new("Internal/MostRecentlyUsedSessions", SettingsContainer, () => new MRUDictionary());
+
     static InternalSettings()
     {
         profile = LoadProfile(true);
         SettingsContainer.CurrentProfile = profile;
     }
 
-    public static SettingsContainer SettingsContainer { get; } = new();
+    /// <summary>
+    /// Loads a copy of the internal settings from the file.
+    /// </summary>
+    public static SettingsProfile LoadProfileCopy()
+    {
+        return LoadProfile(false);
+    }
 
     /// <summary>
     /// Loads the settings from the file.
     /// </summary>
-    /// <returns></returns>
     private static SettingsProfile LoadProfile(bool registerProfile)
     {
         return SettingsContainer.LoadSettingsProfile(EditorPath.InternalConfigPath, false, null, registerProfile) ?? SettingsContainer.CreateSettingsProfile(false);
@@ -31,6 +41,11 @@ public static class InternalSettings
     /// </summary>
     public static void SaveProfile()
     {
+        // Special case for MRU: we always reload the latest version from the file.
+        // Actually modifying and saving MRU is done in a specific class.
+        var profileCopy = LoadProfileCopy();
+        var mruList = MostRecentlyUsedSessions.GetValue(profileCopy, true);
+        MostRecentlyUsedSessions.SetValue(mruList);
         SettingsContainer.SaveSettingsProfile(profile, EditorPath.InternalConfigPath);
     }
 }
