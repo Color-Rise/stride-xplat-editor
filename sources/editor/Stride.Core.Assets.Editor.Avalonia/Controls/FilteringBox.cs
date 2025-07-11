05175965e0c4e351d948be4622214cfba601cd23
Nicolas Musset <nicolas@color-rise.xyz>
2025-07-11 16:33:25 +0200

    WIP FilteringBox 2

--- sources/editor/Stride.Core.Assets.Editor.Avalonia/Controls/FilteringBox.cs
+++ sources/editor/Stride.Core.Assets.Editor.Avalonia/Controls/FilteringBox.cs
@@ -9,6 +9,7 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
 using Avalonia.Metadata;
 
+// FIXME xplat-editor move to Presentation ?
 namespace Stride.Core.Assets.Editor.Avalonia.Controls;
 
 [TemplatePart("PART_ListBox", typeof(ListBox))]
